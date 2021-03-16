5b9125f4c6478e542d536cfe9ff73d8cb309298f
Victor Hugo <victormesquita2190@gmail.com>
2021-03-16 20:02:10 -0300

    OrderItem, many-tomany association with extra atributes

--- src/main/java/com/dungeonsedragons/course/config/TestConfig.java
+++ src/main/java/com/dungeonsedragons/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.dungeonsedragons.course.entities.Category;
 import com.dungeonsedragons.course.entities.Order;
+import com.dungeonsedragons.course.entities.OrderItem;
 import com.dungeonsedragons.course.entities.Product;
 import com.dungeonsedragons.course.entities.User;
 import com.dungeonsedragons.course.enums.OrderStatus;
 import com.dungeonsedragons.course.repositories.CategoryRepository;
+import com.dungeonsedragons.course.repositories.OrderItemRepository;
 import com.dungeonsedragons.course.repositories.OrderRepository;
 import com.dungeonsedragons.course.repositories.ProductRepository;
 import com.dungeonsedragons.course.repositories.UserRepository;
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Autowired
 	private ProductRepository productRepository;
@@ -68,6 +73,12 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	}
 }

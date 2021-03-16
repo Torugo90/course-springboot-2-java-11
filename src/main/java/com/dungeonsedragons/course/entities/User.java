3b38b15dbb042910a93e0ba8737ab398587ffa74
Victor Hugo <victormesquita2190@gmail.com>
2021-03-15 22:58:04 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/dungeonsedragons/course/entities/User.java
+++ src/main/java/com/dungeonsedragons/course/entities/User.java
@@ -1,27 +1,38 @@
 package com.dungeonsedragons.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
-		
+
 	}
-	
+
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -30,6 +41,7 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
+
 	private String password;
 
 	public Long getId() {
@@ -72,6 +84,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,6 +112,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
---
+++ src/main/java/com/dungeonsedragons/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.dungeonsedragons.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dungeonsedragons.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
